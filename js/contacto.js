63a64c51ba7d691b62b7fc09d8d9a640e9bef274
Luis Alvarez R <l.ant.1.4r@gmail.com>
2026-09-04 20:29:40 -0400

    FEAT: Creacion de formulario login & registro
    Adicionalmente
    - Validador general ahora solo se encarga de verificar que los campos no esten vacios
    - Ahora hay un validador aparte para el correo
    - Correcion de enlace en menu.css

--- js/contacto.js
+++ js/contacto.js
@@ -2,5 +2,5 @@ const f_contacto = document.querySelector('#form-contacto');
 
 f_contacto.addEventListener('submit', function(e) {
     e.preventDefault();
-    validarCamposGeneral(f_contacto, '¡Enviado correctamente!');
+    validarCamposGeneral(f_contacto/*, '¡Enviado correctamente!'*/);
 });
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,12 +1,14 @@
-function validarCamposGeneral(formulario, mensaje){
+function validarCamposGeneral(formulario/*, mensaje*/){
 
     const camposRequeridos = formulario.querySelectorAll('[required]');
-    const patronCorreo = /^[^\s@]+@(duocuc\.cl|profesor\.duoc\.cl|gmail\.com)$/;
-    
     
     let formularioValido = true;
 
-    camposRequeridos.forEach(function(campo) {
+    camposRequeridos.forEach(campo => {
+        if (campo.offsetParent === null) {
+            return;
+        }
+
         if(campo.value.trim() === '') {
             campo.classList.add('campo-error');
         formularioValido = false;
@@ -14,20 +16,31 @@ function validarCamposGeneral(formulario, mensaje){
             campo.classList.remove('campo-error');
         }
     });
-
-    const correo = formulario.querySelector('#correo');
-
-    if (!patronCorreo.test(correo.value.trim())) {
-        correo.classList.add('campo-error');
-        formularioValido = false;
-    } else {
-        correo.classList.remove('campo-error');
-    }
-
+    /*
     const mensajeConfirmacion = formulario.querySelector('#mensaje-confirmacion');
+    
     if(formularioValido) {
         mensajeConfirmacion.textContent = mensaje;
     } else {
         mensajeConfirmacion.textContent = '';
     } 
+    */
+    return formularioValido;
 }
+
+function validarCorreo(correo){
+
+    const patronCorreo = /^[^\s@]+@(duocuc\.cl|profesor\.duoc\.cl|gmail\.com)$/;
+
+    let correoValido = true;
+
+    if (!patronCorreo.test(correo.value.trim())) {
+        correo.classList.add('campo-error');
+        correoValido = false;
+    } else {
+        correo.classList.remove('campo-error');
+    }
+
+    return correoValido;
+}
+ 
