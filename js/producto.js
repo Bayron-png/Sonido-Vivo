ace63708a09595c90f72a3ceb02379750f5bfdcb
Luis Alvarez R <l.ant.1.4r@gmail.com>
2026-09-06 20:37:33 -0300

    REFACTOR: Cambio de checkout.html a pedido.html

--- js/producto.js
+++ js/producto.js
@@ -3,5 +3,5 @@ const btnAgregar = document.querySelector("#btn-agregar");
 btnAgregar.addEventListener('click',function(){
    
     alert('Producto agregado al carrito');
-    window.location.href = '../checkout.html';
+    window.location.href = '../pedido.html';
 });
